07f99038af14005065d1abb87b67c808f265e822
edus44 <eduhhg@gmail.com>
2017-02-28 19:33:04 +0100

    working on figures

--- src/lib/figureMaps.js
+++ src/lib/figureMaps.js
@@ -38,6 +38,39 @@ export const HEAD_LEFT = [
     [2,2],
 ]
 
+export const HEAD_MOUTH_UP = [
+    [0,3],
+    [2,3],
+    [1,2],
+    [2,2],
+    [0,1],
+    [3,1],
+]
+export const HEAD_MOUTH_RIGHT = [
+    [0,0],
+    [0,2],
+    [1,1],
+    [1,2],
+    [2,0],
+    [2,3],
+]
+export const HEAD_MOUTH_DOWN = [
+    [0,0],
+    [2,0],
+    [1,1],
+    [2,1],
+    [0,2],
+    [3,2],
+]
+export const HEAD_MOUTH_LEFT = [
+    [3,0],
+    [3,2],
+    [1,0],
+    [1,3],
+    [2,1],
+    [2,2],
+]
+
 export const BODY_UP = [
     [1,0],
     [2,0],
@@ -70,6 +103,115 @@ export const BODY_LEFT = [
     [3,2],
     [3,1],
 ]
+
+export const BODY_CORNER_UP_RIGHT = [
+    [2,1],
+    [3,1],
+    [1,2],
+    [3,2],
+    [1,3],
+    [2,3],
+]
+export const BODY_CORNER_UP_LEFT = [
+    [0,1],
+    [1,1],
+    [0,2],
+    [2,2],
+    [1,3],
+    [2,3],
+]
+export const BODY_CORNER_DOWN_RIGHT = [
+    [1,0],
+    [2,0],
+    [1,1],
+    [3,1],
+    [2,2],
+    [3,2],
+]
+export const BODY_CORNER_DOWN_LEFT = [
+    [1,0],
+    [2,0],
+    [0,1],
+    [2,1],
+    [0,2],
+    [1,2],
+]
+
+export const BODY_CORNER_RIGHT_UP = BODY_CORNER_DOWN_LEFT
+export const BODY_CORNER_RIGHT_DOWN = BODY_CORNER_UP_LEFT
+export const BODY_CORNER_LEFT_UP = BODY_CORNER_DOWN_RIGHT
+export const BODY_CORNER_LEFT_DOWN = BODY_CORNER_UP_RIGHT
+
+
+export const BODY_FOOD_CORNER_UP_RIGHT = [
+    [2,1],
+    [3,1],
+    [1,2],
+    [3,2],
+    [1,3],
+    [2,3],
+    [3,3],
+]
+export const BODY_FOOD_CORNER_UP_LEFT = [
+    [0,1],
+    [1,1],
+    [0,2],
+    [2,2],
+    [1,3],
+    [2,3],
+    [0,3],
+]
+export const BODY_FOOD_CORNER_DOWN_RIGHT = [
+    [3,0],
+    [1,0],
+    [2,0],
+    [1,1],
+    [3,1],
+    [2,2],
+    [3,2],
+]
+export const BODY_FOOD_CORNER_DOWN_LEFT = [
+    [0,0],
+    [1,0],
+    [2,0],
+    [0,1],
+    [2,1],
+    [0,2],
+    [1,2],
+]
+
+export const BODY_FOOD_CORNER_RIGHT_UP = BODY_FOOD_CORNER_DOWN_LEFT
+export const BODY_FOOD_CORNER_RIGHT_DOWN = BODY_FOOD_CORNER_UP_LEFT
+export const BODY_FOOD_CORNER_LEFT_UP = BODY_FOOD_CORNER_DOWN_RIGHT
+export const BODY_FOOD_CORNER_LEFT_DOWN = BODY_FOOD_CORNER_UP_RIGHT
+
+
+export const BODY_FOOD_UP = [
+    [1,0],
+    [2,0],
+    [0,2],
+    [1,2],
+    [3,2],
+    [0,1],
+    [2,1],
+    [3,1],
+    [1,3],
+    [2,3],
+]
+export const BODY_FOOD_RIGHT = [
+    [1,0],
+    [2,0],
+    [0,1],
+    [1,1],
+    [3,1],
+    [0,2],
+    [2,2],
+    [3,2],
+    [1,3],
+    [2,3],
+]
+export const BODY_FOOD_DOWN = BODY_FOOD_RIGHT
+export const BODY_FOOD_LEFT = BODY_FOOD_UP
 
 
 export const TAIL_UP = [
